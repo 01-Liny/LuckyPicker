e331b8b93d0eb79c7a916938f220d647988a43bf
XSAM <xsambundy@gmail.com>
2017-04-30 16:52:32 +0800

    feat: color/passcode random

---
+++ LuckyPicker/ViewController/PickPasscodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  PickPasscodeViewController.h
+//  LuckyPicker
+//
+//  Created by BangshengXie on 30/04/2017.
+//  Copyright © 2017 BangshengXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickPasscodeViewController : UIViewController
+
+@end
---
+++ LuckyPicker/ViewController/PickPasscodeViewController.m
@@ -0,0 +1,31 @@
+//
+//  PickPasscodeViewController.m
+//  LuckyPicker
+//
+//  Created by BangshengXie on 30/04/2017.
+//  Copyright © 2017 BangshengXie. All rights reserved.
+//
+
+#import "PickPasscodeViewController.h"
+
+@interface PickPasscodeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+
+@end
+
+@implementation PickPasscodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
