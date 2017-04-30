3ec1bcfc3221a177fdc17746205c1921ffb29ede
XSAM <xsambundy@gmail.com>
2017-04-30 10:22:11 +0800

    chore: add category for UIColor to hex

---
+++ LuckyPicker/Models/Color/UIColor+Hex.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Hex.h
+//  LuckyPicker
+//
+//  Created by BangshengXie on 30/04/2017.
+//  Copyright © 2017 BangshengXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Hex)
+
++ (UIColor*)colorWithHexString:(NSString*)hexString;
+
+@end
